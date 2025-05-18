450f2df9f4da7bee79da198321bdb0269fd81fbe
Ash Hellwig <ahellwig.dev@gmail.com>
2025-05-17 20:52:28 -0600

    init

---
+++ jobappfiller/tools/parse_job_config.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2025 Ash Hellwig <ahellwig.dev@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from jobappfiller.util.logger import setup_logger
+
+logger = setup_logger()
+
+
+def add_one(number: int) -> int:
+    """Increments the given number by one.
+
+    Args:
+        number (int): Number to increment.
+
+    Returns:
+        int: Incremented number.
+    """
+    logger.info('Running add_one')
+    return number + 1
